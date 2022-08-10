e8fe5fe1ebf2aa2f55c8b19af64d6335b8c1fce3
Jose Gregorio <joze.gtablante@gmail.com>
2022-08-10 20:48:56 -0300

    primer commit
    hasta esta parte he consumido la app, usando Fetch, he creado una card, donde he imprimido todos los personajes, adicional dos botones adelante atras para mostrar los demas personajes. la card se reinicia cada vez que se actualiza

---
+++ index.js
@@ -0,0 +1,75 @@
+const URI = "https://rickandmortyapi.com/api/character";
+
+const divCard = document.getElementById("cards-container")
+const btnSiguiente = document.getElementById("siguiente")
+const btnAtras = document.getElementById("atras")
+
+let UrlSiguiente = null
+let UrlAtras = null
+
+btnAtras.disabled = true
+btnSiguiente.disabled = true
+
+btnSiguiente.addEventListener('click',()=>{
+    cargarDatos(UrlSiguiente)
+})
+
+btnAtras.addEventListener('click',()=>{
+    cargarDatos(UrlAtras)
+})
+
+cargarDatos(URI)
+
+function cargarDatos(URL) {
+    fetch(URL)
+    .then(respuesta => respuesta.json())
+    .then(data => {
+        const personajes = data.results
+        const pagSiguiente = data.info.next
+        const pagAtras = data.info.prev
+        // console.log(data.info.next)
+        imprimirPersonajes(personajes)
+        // imprimirPersonajes(pagSiguiente)
+
+        if(pagAtras !=null){
+            UrlAtras = pagAtras
+            btnAtras.disabled = false
+        }else{
+            UrlSiguiente = pagSiguiente
+            btnSiguiente.disabled = true
+        }
+
+        if(pagSiguiente !=null){
+            UrlSiguiente = pagSiguiente
+            btnSiguiente.disabled = false
+        }else{
+            UrlSiguiente = pagSiguiente
+            btnSiguiente.disabled = true
+        }
+
+    })
+} 
+
+function imprimirPersonajes(arrayPersonajes) { 
+    divCard.innerHTML= ''
+    arrayPersonajes.forEach(personaje => {
+    let card = document.createElement("div");
+    card.className = "card p-0 bg-dark text-light";
+    card.style.width = "18rem"
+    card.innerHTML = `<img class="card-img-top" src="${personaje.image}" alt="">
+        <div class="card-body">
+        <p class="card-text"><strong>${personaje.id}</strong> Nombre: ${personaje.name}</p>
+        </div>`
+    divCard.appendChild(card);
+  });
+}
+
+// CONSUMO DE API APLICANDO CATCH(PARA CACTURAR ERRORES) AND FINALLY (COMO QUEREMOS QUE SE COMPORTE NUESTRA APP LUEGO DE QUE REALICE EL FETCH)
+// fetch(`https://rickandmortyapi.com/api/character`)
+// .then(response => response.json())
+// .then(datos => {
+//     console.log(datos.results[1])
+
+// })
+// .catch(error => console.warn(error.message))
+// .finally(console.log("finalizo el fetch"))
